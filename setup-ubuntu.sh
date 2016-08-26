e5abdc2ff90e47ab2afb04ca2a039c67c6a47f41
VrsajkovIvan33 <vrsajkov.ivan@gmail.com>
2016-08-26 12:13:08 +0200

    Remove install of erlang 18

--- setup-ubuntu.sh
+++ setup-ubuntu.sh
@@ -4,9 +4,9 @@ sudo apt-get update
 sudo apt-get install wget
 
 #instal erlang
-wget https://packages.erlang-solutions.com/erlang/esl-erlang/FLAVOUR_1_general/esl-erlang_18.3-1~ubuntu~trusty_amd64.deb
-sudo apt-get install -f
-sudo dpkg -i esl-erlang_18.3-1~ubuntu~trusty_amd64.deb
+#wget https://packages.erlang-solutions.com/erlang/esl-erlang/FLAVOUR_1_general/esl-erlang_18.3-1~ubuntu~trusty_amd64.deb
+#sudo apt-get install -f
+#sudo dpkg -i esl-erlang_18.3-1~ubuntu~trusty_amd64.deb
 
 #install elixir
 wget https://packages.erlang-solutions.com/erlang-solutions_1.0_all.deb && sudo dpkg -i erlang-solutions_1.0_all.deb
